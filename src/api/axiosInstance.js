914844ac5bf715bfe8d4f9e1f44f406f81fb021d
aaryan-jais <95408432+aaryan-jais@users.noreply.github.com>
2026-01-24 00:27:25 +0530

    changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 const api = import.meta.env.VITE_API_URL;
+// const api_local= import.meta.env.VITE_LOCAL_URL;
+
 
 const axiosInstance = axios.create({
-  baseURL: `${api}/api`,
+  baseURL: `${api_local}/api`,
 });
 
 export default axiosInstance;
--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 const api = import.meta.env.VITE_API_URL;
+// const api_local= import.meta.env.VITE_LOCAL_URL;
+
 
 
 const productSlice = createSlice({
@@ -14,7 +16,7 @@ const productSlice = createSlice({
 export const { setProducts } = productSlice.actions;
 
 export const fetchProducts = () => async (dispatch) => {
-  const res = await axios.get(`${api}/api/products`);
+  const res = await axios.get(`${api_local}/api/products`);
   dispatch(setProducts(res.data));
 };
 
