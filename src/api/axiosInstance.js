0b806ab6469837613a7fb64cf8f1b2f8b4ea6f43
aaryan-jais <95408432+aaryan-jais@users.noreply.github.com>
2026-01-24 00:34:09 +0530

    changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-const api = import.meta.env.VITE_API_URL;
+const api_URL = import.meta.env.VITE_API_URL;
 // const api_local= import.meta.env.VITE_LOCAL_URL;
 
 
 const axiosInstance = axios.create({
-  baseURL: `${api_local}/api`,
+  baseURL: `${api_URL}/api`,
 });
 
 export default axiosInstance;
